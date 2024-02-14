a0f5636a14a82d3b9dbd47e7e57a8b529a1872a2
Henrique <nuneshp2@gmail.com>
2024-02-14 19:56:06 -0300

    estudos

---
+++ src/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,16 @@
+package javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+    public void subtraiDoisNumeros(){
+        System.out.println(20-2);
+    }
+
+    public void multiplicaDoisNumeros(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+}
