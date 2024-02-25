ffa36151ff05d16eece76c088c8568fbedec809d
Henrique <nuneshp2@gmail.com>
2024-02-25 14:52:37 -0300

    Inclusao

---
+++ src/javacore/Bintroducaometodos/Test/CalculadoraTest05.java
@@ -0,0 +1,14 @@
+package javacore.Bintroducaometodos.Test;
+
+import javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int [] numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(1,2,3,4,5,6,7);
+    }
+
+
+}
