65090beb0ae4c52c5404edab5ee1b56efdad2f82
Henrique <nuneshp2@gmail.com>
2024-03-06 23:37:18 -0300

    POO

---
+++ src/javacore/JmodificadorFinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package javacore.JmodificadorFinal.test;
+
+import javacore.JmodificadorFinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
