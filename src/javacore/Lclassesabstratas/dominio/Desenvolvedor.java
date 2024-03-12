6b1c48e3e2006961938f0c92b530f9f7f73a3709
Henrique <nuneshp2@gmail.com>
2024-03-12 18:24:48 -0300

    Classes Abstratas

---
+++ src/javacore/Lclassesabstratas/dominio/Desenvolvedor.java
@@ -0,0 +1,20 @@
+package javacore.Lclassesabstratas.dominio;
+
+public class Desenvolvedor extends Funcionario{
+    public Desenvolvedor(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public String toString() {
+        return "Desenvolvedor{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    @Override
+    public void calculaBonus() {
+        this.salario = this.salario + this.salario * 0.05;
+    }
+}
---
+++ src/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package javacore.Lclassesabstratas.test;
+
+import javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import javacore.Lclassesabstratas.dominio.Funcionario;
+import javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Lucas", 5000);
+        System.out.println(gerente);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Henrique", 10000);
+        System.out.println(desenvolvedor);
+    }
+
+}
