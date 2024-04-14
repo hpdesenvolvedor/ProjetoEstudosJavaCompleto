aaf39c85681522ed5cee71ad1f8922630a8700c2
Henrique <nuneshp2@gmail.com>
2024-04-14 17:31:35 -0300

    Classes Utilitarias - IO

---
+++ src/javacore/Wnio/test/PathTest01.java
@@ -0,0 +1,17 @@
+package javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest01 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\01. DEV\\PROJETOS\\CursoJavaCompleto\\file.txt");
+        Path p2 = Paths.get("C:","\\01. DEV\\PROJETOS\\CursoJavaCompleto\\file.txt");
+        Path p3 = Paths.get("C:\\01. DEV\\PROJETOS\\CursoJavaCompleto","file.txt");
+        Path p4 = Paths.get("C:\\01. DEV\\PROJETOS\\CursoJavaCompleto\\file.txt");
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+}
---
+++ src/javacore/Wnio/test/PathTest02.java
@@ -0,0 +1,28 @@
+package javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class PathTest02 {
+    public static void main(String[] args) throws IOException {
+        Path pastaPath = Paths.get("pasta");
+        if (Files.notExists(pastaPath)){
+            Path pastaDirectory = Files.createDirectory(pastaPath);
+        }
+
+        Path subPastaPath = Paths.get("pasta/subpasta/subsubpasta");
+        Path subPastaDirectory = Files.createDirectories(subPastaPath);
+
+        Path filePath = Paths.get(subPastaPath.toString(),"file.txt");
+        if (Files.notExists(filePath)) {
+            Path filePathCreated = Files.createFile(filePath);
+        }
+
+        Path source = filePath;
+        Path target = Paths.get(filePath.getParent().toString(),"file_rename.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+    }
+}
