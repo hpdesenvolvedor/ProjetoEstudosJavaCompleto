1d1977a2b689850cfb5ff91607aa85385b1c5e97
Henrique <nuneshp2@gmail.com>
2024-04-19 01:23:42 -0300

    Colecoes - equals

---
+++ src/javacore/Ycolecoes/test/EqualsTest01.java
@@ -0,0 +1,15 @@
+package javacore.Ycolecoes.test;
+
+import javacore.Ycolecoes.dominio.Smartphone;
+
+// EQUALS VERIFICANDO SE A REFERÊNCIA É IGUAL. CASO SEJA (TRUE) SE NÃO (FALSE)
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone phone11 = new Smartphone("1A2B3C", "Iphone");
+        Smartphone phone12 = phone11;
+        Smartphone fusion = new Smartphone("1A2B3C", "Motorola");
+        Smartphone s23 = new Smartphone("1A2B3C", "Samsung");
+
+        System.out.println(phone11.equals(phone12) );
+    }
+}
