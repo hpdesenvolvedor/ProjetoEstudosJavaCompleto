57f6ac5110463b257d6a0308a646fc8df0bd3d02
Henrique <nuneshp2@gmail.com>
2024-04-19 22:51:43 -0300

    Colecoes

--- src/javacore/Ycolecoes/test/EqualsTest01.java
+++ src/javacore/Ycolecoes/test/EqualsTest01.java
@@ -6,7 +6,8 @@ import javacore.Ycolecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone phone11 = new Smartphone("1A2B3C", "Iphone");
-        Smartphone phone12 = phone11;
+        Smartphone phone12 = new Smartphone("1A2B3C", "Iphone");
+
         Smartphone fusion = new Smartphone("1A2B3C", "Motorola");
         Smartphone s23 = new Smartphone("1A2B3C", "Samsung");
 
