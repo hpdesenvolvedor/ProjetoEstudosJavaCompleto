007d5f85c35d35eee75740c54b80ca613deb93c6
Henrique <nuneshp2@gmail.com>
2024-05-12 16:09:44 -0300

    Stream - Gerando_streams

---
+++ src/javacore/ZZEstreams/test/StreamsTest10.java
@@ -0,0 +1,28 @@
+package javacore.ZZEstreams.test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StreamsTest10 {
+    public static void main(String[] args) {
+        Stream.iterate(1, n -> n + 2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("---------------------");
+
+        //0,1,1,2,3,5,8,13,21,34
+        //0 1
+        // (0,1) (1,1) (1,2) (2,3) (3,5) (5,8)
+        Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+                .limit(10)
+                .forEach(a -> System.out.println(Arrays.toString(a)));
+
+        System.out.println("---------------------");
+
+        Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+                .limit(10)
+                .map(a -> a[0])
+                .forEach(System.out::println);
+    }
+}
