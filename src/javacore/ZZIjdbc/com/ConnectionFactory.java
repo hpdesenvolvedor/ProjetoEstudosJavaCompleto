48ec89a8e7ea4bf45bf7c5144cda5296b33239fb
Henrique <nuneshp2@gmail.com>
2024-06-09 18:47:11 -0300

    JBDC

--- src/javacore/ZZIjdbc/com/ConnectionFactory.java
+++ src/javacore/ZZIjdbc/com/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package javacore.ZZIjdbc.com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+
     //java.sql = Connection, Statement, Result, DriverManager
+    public static Connection getConnection(){
+        String url = "jbdc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+        try {
+            DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
---
+++ src/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,11 @@
+package javacore.ZZIjdbc.test;
+
+import javacore.ZZIjdbc.com.ConnectionFactory;
+
+import java.sql.Connection;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
